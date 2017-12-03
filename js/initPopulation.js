62e6e194673f9ba3eb5459a44ffee48c46ef6139
adambrezina <adam.brezina@icloud.com>
2017-12-03 02:05:51 +0100

    added initPopulation.js

---
+++ js/initPopulation.js
@@ -0,0 +1,46 @@
+class InitPopulation {
+  constructor(length, type) {
+    this.type = type;
+    this.length = length;
+    this.max = 1;
+    this.min = 0;
+    this.genotyp = [this.length];
+  }
+
+  setLimits(min, max){
+    this.min = min;
+    this.max = max;
+  }
+
+  getPopulation(){
+    if(this.type == "BINARY")
+    {
+      for(var i = 0; i < this.length; i++)
+      {
+        this.genotyp[i] = Math.round(Math.random()) * (this.max - this.min) + this.min;
+      }
+    }
+    else if (this.type == "INTEGER")
+    {
+      for(var i = 0; i < this.length; i++)
+      {
+        this.genotyp[i] = Math.round(Math.random() * (this.max - this.min) + this.min);
+      }
+    }
+    else if (this.type == "FLOAT")
+    {
+      for(var i = 0; i < this.length; i++)
+      {
+        this.genotyp[i] = Math.random() * (this.max - this.min) + this.min;
+      }
+    }
+
+    console.log(this.genotyp);
+  }
+}
+
+var a = new InitPopulation(5, "INTEGER");
+a.setLimits(5,10);
+a.getPopulation();
+a.getPopulation();
+a.getPopulation();
