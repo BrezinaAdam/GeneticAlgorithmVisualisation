d69edcb39425dd6c7f6d2cb629b204623a134148
milan <milan.tkacik@student.tuke.sk>
2017-12-04 19:53:08 +0100

    neural network

---
+++ js/dataset.js
@@ -0,0 +1,22 @@
+'using strict';
+
+class Dataset
+{
+  static generate(from, to, step)
+  {
+    let res = [2];
+    res[0] = [];
+    res[1] = [];
+
+    let curr = from;
+    do
+    {
+      res[0].push(curr);
+      res[1].push(Math.sin(curr));
+      curr += step;
+    }
+    while (curr < to);
+
+    return res;
+  }
+}
