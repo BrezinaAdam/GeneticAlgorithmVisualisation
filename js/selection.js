7f7237673f3efc92cbf08c9cd68ba098bcbbc404
adambrezina <adam.brezina@icloud.com>
2017-12-05 00:24:18 +0100

    selection.js roulette done

--- js/selection.js
+++ js/selection.js
@@ -1,27 +1,51 @@
 class Selection {
-  constructor() {
 
-  }
+  getOne(){
+    let sum = 0;
+    let rand = Math.random()*this.totalFittness;
+    let index = 0;
 
-  getFitness(pop){
-    var fitness = [pop.legth];
-
-    for(var i = 0; i < pop.length; i++)
-    {
-      fitness[i] = Math.random()*10;
+    while(rand > sum){
+      sum += this.fitness[index];
+      index++;
     }
 
-    return fitness;
+    return this.population[index-1];
+
   }
 
-  getRoulete(pop){
-    var fitness = getFitness(pop);
+  getRoulete(){
+    this.selected = [this.population.length];
 
-    //...
+    for(let i = 0; i < this.population.length; i = i + 2){
+      this.selected[i] = getOne();
+      do{
+        this.selected[i+1] = getOne();
+      }while (this.selected[i] == this.selected[i+1]);
+    }
   }
 
-  getBinary(pop){
+  getBinary(){
+    this.selected = [this.population.length];
     // something to do
+  }
+
+  getSelectedParrents(pop, fitness type){
+    this.population = pop;
+    this.fitness = fitness;
+    this.type = type;
+    this.totalFittness = fitness.reduce((a, b) => { return a + b; });
+
+    if(type == "ROULETTE"){
+      this.getRoulete();
+
+      return this.selected;
+    }
+
+    else if(type == "BINARY"){
+      this.getBinary();
+      return this.selected;
+    }
   }
 }
 
